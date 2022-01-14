85fde843591db2228ce8ffc69c44359ca57e8d4f
Chaeguevara <dfso2@naver.com>
2022-01-14 20:54:04 +0900

    SPA Part1

--- React/confusion/src/components/MainComponent.js
+++ React/confusion/src/components/MainComponent.js
@@ -1,10 +1,14 @@
 import Menu from './MenuComponent';
 import Home from './HomeComponent';
-import {DISHES} from '../shared/dishes';
+import Contact from './ContactComponent';
 import React, { Component } from 'react';
 import Header from './HeaderComponent';
 import DishDetail from './DishdetailComponent';
 import Footer from './FooterComponent';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
+import {DISHES} from '../shared/dishes';
 import {Routes, Route, Navigate} from 'react-router-dom';
 import { Nav } from 'reactstrap';
 
@@ -17,6 +21,9 @@ class Main extends Component {
 
         this.state={
             dishes: DISHES,
+            commnets : COMMENTS,
+            promotions : PROMOTIONS,
+            leaders : LEADERS
         };
     }
 
@@ -25,7 +32,10 @@ class Main extends Component {
         
         const HomePage = () =>{
             return(
-                <Home />
+                <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                leader ={this.state.leaders.filter((leader) => leader.featured)[0]} 
+                />
             );
         }
         return (
@@ -35,6 +45,7 @@ class Main extends Component {
               <Routes>
                   <Route path="/home" element={<HomePage/>} />
                   <Route exact path="/menu" element={<Menu dishes={this.state.dishes} />} />
+                  <Route exact path="/contactus" element={<Contact />} />
                   <Route path="*" element={<Navigate to="/home" />}/>
               </Routes>
               <Footer/>
